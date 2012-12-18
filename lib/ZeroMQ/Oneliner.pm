f6d2c8c3f29f623a987ecf00240bf5c3c5dc8ac4
x86_64 <x86mail@gmail.com>
2012-12-18 14:28:37 +0400

    initial

---
+++ lib/ZeroMQ/Oneliner.pm
@@ -0,0 +1,119 @@
+package ZeroMQ::Oneliner;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+ZeroMQ::Oneliner - The great new ZeroMQ::Oneliner!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use ZeroMQ::Oneliner;
+
+    my $foo = ZeroMQ::Oneliner->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+x86, C<< <x86mail at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-zeromq-oneliner at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=ZeroMQ-Oneliner>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc ZeroMQ::Oneliner
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=ZeroMQ-Oneliner>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/ZeroMQ-Oneliner>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/ZeroMQ-Oneliner>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/ZeroMQ-Oneliner/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 x86.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program.  If not, see
+L<http://www.gnu.org/licenses/>.
+
+
+=cut
+
+1; # End of ZeroMQ::Oneliner
