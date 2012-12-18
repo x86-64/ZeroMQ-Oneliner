0de951f72fcb168bdd07e2169506c9ed4e675b76
x86_64 <x86mail@gmail.com>
2012-12-18 15:19:14 +0400

    Working code

--- lib/ZeroMQ/Oneliner.pm
+++ lib/ZeroMQ/Oneliner.pm
@@ -3,6 +3,7 @@ package ZeroMQ::Oneliner;
 use 5.006;
 use strict;
 use warnings FATAL => 'all';
+use ZeroMQ qw(:all);
 
 =head1 NAME
 
@@ -16,6 +17,29 @@ Version 0.01
 
 our $VERSION = '0.01';
 
+our $types = {
+	"push" => ZMQ_PUSH,
+	"pull" => ZMQ_PULL,
+	"req"  => ZMQ_REQ,
+	"rep"  => ZMQ_REP,
+	"pub"  => ZMQ_PUB,
+	"sub"  => ZMQ_SUB,
+};
+our $opts = {
+	"hwm"         => ZMQ_HWM,
+	"swap"        => ZMQ_SWAP,
+	"identity"    => ZMQ_IDENTITY,
+	"subscribe"   => ZMQ_SUBSCRIBE,
+	"rate"        => ZMQ_RATE,
+	"recovery"    => ZMQ_RECOVERY_IVL,
+	"sndbuf"      => ZMQ_SNDBUF,
+	"rcvbuf"      => ZMQ_RCVBUF,
+	"linger"      => ZMQ_LINGER,
+	"reconnect"   => ZMQ_RECONNECT_IVL,
+	"backlog"     => ZMQ_BACKLOG,
+};
+
+my $ctx = ZeroMQ::Context->new();
 
 =head1 SYNOPSIS
 
@@ -39,14 +63,48 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =cut
 
-sub function1 {
+sub TIEHANDLE {
+    (
+        ( defined( $_[1] ) && UNIVERSAL::isa( $_[1], __PACKAGE__ ) )
+        ? $_[1]
+        : shift->new(@_)
+    );	
 }
 
-=head2 function2
+sub PRINT {    my $self = shift; *$self->{socket}->send(@_);      } 
+sub READLINE { my $self = shift; my $msg = *$self->{socket}->recv(); $msg->data(); }
+sub CLOSE {    my $self = shift; *$self->{socket}->close();      }
 
-=cut
-
-sub function2 {
+sub new {
+	my ($class, $name) = @_;
+	
+	my $self = bless \do { local *FH }, $class;
+	tie *$self, $class, $self;
+	
+	my ($socket);
+	
+	if($name =~ m@(bind|connect)-((?:inproc|ipc|tcp|pgm|epgm)://[[:alnum:].*]+:[0-9]+)/(re[qp]|push|pull|[ps]ub)[?]?([[:alnum:]=&]+)?@i){
+		my ($bc, $zmq_address, $s_type, $s_options) = ($1, $2, $3, $4);
+		
+		$socket = $ctx->socket($types->{$s_type});
+		
+		if($s_options){
+			foreach my $option (split /&/, $s_options){
+				my ($k, $v) = split /=/, $option;
+				
+				$k = $opts->{$k};
+				$v = int($v) if $v =~ /^\d+$/;
+				
+				$socket->setsockopt($k, $v);
+			}
+		}
+		
+		if($bc eq "bind"){    $socket->bind($zmq_address);    }
+		if($bc eq "connect"){ $socket->connect($zmq_address); }
+		
+		*$self->{socket} = $socket;
+	}
+	return $self;
 }
 
 =head1 AUTHOR
